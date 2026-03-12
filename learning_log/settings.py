7c49fd3e928f0ca61971c88bbe525bb0bb9f35fa
emmanuel202022 <131202541+emmanuel202022@users.noreply.github.com>
2026-03-12 16:08:52 -0300

    sin env

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from environ import Env
-env = Env()
-Env.read_env()
-ENVIRONMENT = env('ENVIROMENT',default='production')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
